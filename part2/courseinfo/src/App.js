6d8f438d6be38b5c536f15bb0fdabb80fe592445
MiguelParra <miaparralo@unal.edu.co>
2021-12-01 09:11:27 -0500

    Exercise 2.4: Course information step9

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -11,7 +11,7 @@ const Total = ({ course }) => {
   const TotalExercises = course.parts.reduce((sum, exercise) => {
     //console.log("what is happening", sum, exercise.exercises)
     return sum + exercise.exercises
-  }, 0 )
+  }, 0)
   //console.log("la suma es",TotalExercises)
   return (
     <p> <strong> total of {TotalExercises} exercises </strong> </p>
@@ -42,37 +42,68 @@ const Course = (props) => {
       <Total course={props.course} />
     </div>
   )
+};
+
+const Courses = ({ courses }) => {
+  //console.log("The courses are:", courses)
+  return (
+    <div>
+      {courses.map(course => <Course key={course.id} course={course} />)}
+    </div>
+  )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      <Courses courses={courses} />
+    </div>
+  )
 
 
 }
