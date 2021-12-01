79d34c97299551a3e945371012c1a78cca470afb
MiguelParra <miaparralo@unal.edu.co>
2021-12-01 12:01:27 -0500

    Exercise 2.5: separate module

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,48 +1,6 @@
 import React from 'react'
+import Course from './components/Course.js'
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-
-  const TotalExercises = course.parts.reduce((sum, exercise) => {
-    //console.log("what is happening", sum, exercise.exercises)
-    return sum + exercise.exercises
-  }, 0)
-  //console.log("la suma es",TotalExercises)
-  return (
-    <p> <strong> total of {TotalExercises} exercises </strong> </p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => <Part key={part.id} part={part} />)}
-    </div>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <div>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
-    </div>
-  )
-};
 
 const Courses = ({ courses }) => {
   //console.log("The courses are:", courses)
