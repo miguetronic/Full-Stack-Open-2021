9e3884a5024a2cd99eca767a3c2787dd5b36e65f
MiguelParra <miaparralo@unal.edu.co>
2021-12-02 10:22:24 -0500

    Exercise 2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,11 +9,19 @@ const PersonsDisplay = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-123456', }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchPersons, setSearchPersons] = useState('')
+
+  const handleSearchPerson = (event) => {
+    console.log("Search person is:", event.target.value);
+    setSearchPersons(event.target.value)
+  }
 
   const handleNewName = (event) => {
     //console.log(event.target.value);
@@ -34,15 +42,25 @@ const App = () => {
     const nameObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1
     }
     setPersons(persons.concat(nameObject));
     setNewName('');
     setNewNumber('');
   }
 
+  const personsToShow = (searchPersons === "")
+    ? [...persons]
+    : persons.filter(person => person.name.toLowerCase().includes(searchPersons))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleSearchPerson} value={searchPersons} />
+      </div>
+
+      <h2>add a new</h2>
 
       <form onSubmit={addPerson}>
         <div>
@@ -58,7 +76,7 @@ const App = () => {
 
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <PersonsDisplay key={person.name} person={person} />)}
+        {personsToShow.map(person => <PersonsDisplay key={person.id} person={person} />)}
       </div>
     </div>
   )
