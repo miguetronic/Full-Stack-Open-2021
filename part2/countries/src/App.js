9801d8b5925ee1673be35885278d784822d4dbc6
MiguelParra <miaparralo@unal.edu.co>
2021-12-05 14:09:25 -0500

    2.13*: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,36 +1,84 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryDetails = (props) => {
+  const country = props.country
+  return (
+    <div>
+      <h2 >{country.name.official}</h2>
+      <p>capital {country.capital}</p>
+      <p>population {country.population}</p>
+      <h3>languages</h3>
+      <ul> {Object.values(country.languages).map(language => <li key={language}>{language}</li>)} </ul>
+      <img alt={country.name.official} src={country.flags.png}></img>
+    </div>
+  )
+};
+
 const DisplayCountry = (props) => {
-  console.log("Countries to show are:", props)
-  if (props.countryToShow.length === 0) {
+  const countryToShow = props.countryToShow
+  const searchCountry = props.searchCountry
+
+  if (searchCountry === "") {
+    //    props.handleShowCountry("")
     return (
-      <div>{props.userMessage}</div>
+      <div></div>
     )
   }
-  if (props.countryToShow.length === 1) {
+
+  if (countryToShow.length === 1) {
+    //   props.handleShowCountry("")
     return (
       <div>
-        {props.countryToShow.map(country => {
+        {countryToShow.map(country => {
           return (
-            <div key={country.name.official}>
-              <h2 >{country.name.official}</h2>
-              <p>capital {country.capital}</p>
-              <p>population {country.population}</p>
-              <h3>languages</h3>
-              {Object.values(country.languages).map(language => <ul><li>{language}</li></ul>)}
-              <img alt={country.name.official} src={country.flags.png}></img>
-
-            </div>
+            <CountryDetails key={country.name.official} country={country} />
           )
         }
         )}
       </div>
     )
   }
+
+  else if (countryToShow.length > 10) {
+    //  props.handleShowCountry("")
+    return (
+      <div>
+        <p>Too many matches, specify another filter</p>
+      </div>
+    )
+  }
+
+  else if (countryToShow.length > 1 && countryToShow.length < 10) {
+    return (
+      <div>
+        {countryToShow.map(country => {
+          return (
+            <div key={country.name.official}>
+              {country.name.official}
+              <button onClick={() => props.handleShowCountry(country.name.official)}>
+                show
+              </button>
+              {
+                (props.showCountry === country.name.official) ? (
+                  <CountryDetails key={country.name.official} country={country} />
+                ) : <></>
+              }
+            </div >
+          )
+        }
+        )
+        }
+
+      </div >
+
+    )
+
+  }
+
   return (
     <div>
-      {props.countryToShow.map(country => <p key={country.name.official}>{country.name.official}</p>)}
+      hola
     </div>
   )
 };
@@ -38,47 +86,35 @@ const DisplayCountry = (props) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [searchCountry, setSearchCountry] = useState("")
+  const [showCountry, setShowCountry] = useState("")
 
   useEffect(() => {
     //console.log('effect')
     axios.get('https://restcountries.com/v3.1/all')
       .then(response => {
         //console.log('promise fulfilled')
-        //console.log(response.data)
         setCountries(response.data)
       })
   }, [])
 
   const handleSearchCountry = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setSearchCountry(event.target.value)
   }
-
-  let countryToShow = []
-  let userMessage = ""
-  const countriesList = countries.filter(country => country.name.official.toLowerCase().includes(searchCountry))
-
-  if (searchCountry === "") {
-    countryToShow = []
-  } else if (countriesList.length > 10) {
-    countryToShow = []
-    userMessage = "Too many matches, specify another filter"
-  } else if (countriesList.length > 1) {
-    countryToShow = countriesList
-  } else {
-    countryToShow = countriesList
-    //console.log(countryToShow.length)
+  const handleShowCountry = (event) => {
+    //console.log('event is:', event)
+    setShowCountry(event);
   }
 
 
-  //console.log(countryToShow = countries.filter(country => country.name.official.toLowerCase().includes(searchCountry)))
+  let countryToShow = countries.filter(country => country.name.official.toLowerCase().includes(searchCountry))
 
   return (
     <div>
       find countries
       <input value={searchCountry} onChange={handleSearchCountry}></input>
       <div>
-        <DisplayCountry countryToShow={countryToShow} userMessage={userMessage} />
+        <DisplayCountry countryToShow={countryToShow} searchCountry={searchCountry} handleShowCountry={handleShowCountry} showCountry={showCountry} />
 
       </div>
     </div>
